6674199dfb9eee22dfe9f56b673bf65ea4edd0f0
Bo-Xuan Fan <coffee0127@gmail.com>
2017-01-15 13:22:18 +0800

    v2.0, use readFileSync to avoid callback function

--- renderEngine.js
+++ renderEngine.js
@@ -1,15 +1,12 @@
 var fs = require('fs');
 
 function render(template, data, res) {
-    fs.readFile('./header.html', (err, header) => {
-        fs.readFile('./' + template + '.html', (err, view) => {
-            fs.readFile('./footer.html', (err, footer) => {
-                res.write(header);
-                res.write(view, {});    // {} any information could be passed
-                res.write(footer);
-            });
-        });
-    });
+    var view = fs.readFileSync('./' + template + '.html');
+    var header = fs.readFileSync('./header.html');
+    var footer = fs.readFileSync('./footer.html');
+    res.write(header);
+    res.write(view, {});    // {} any information could be passed
+    res.write(footer);
 }
 
 module.exports.render = render;
