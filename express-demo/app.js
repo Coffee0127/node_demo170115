d7e4ca671a0c7f86aea47cce8f358e1ce26f657d
Bo-Xuan Fan <coffee0127@gmail.com>
2017-01-15 14:35:42 +0800

    rename folder name to make same as package.json

--- express-demo/app.js
+++ express-demo/app.js
@@ -4,9 +4,15 @@ var app = express();
 app.get('/', (req, res) => {
     res.send('hi');
 });
+
 app.get('/user/:name', (req, res) => {
     var name = req.params.name;
-    res.send('Hello ' + name);
+    res.send(`Hello ${name}`);
+});
+
+app.get('/movies/:title?', (req, res) => {
+    var title = req.params.title;
+    res.send(`the title is ${title}`);
 });
 
 app.listen(3000, () => {
