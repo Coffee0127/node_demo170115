c9ef26df2cfa5800d34f983135f9095c9327fd08
Bo-Xuan Fan <coffee0127@gmail.com>
2017-01-15 14:47:25 +0800

    access static files

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var app = express();
 
+// static files
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.send('hi');
 });
 
+// parameters needed
 app.get('/user/:name', (req, res) => {
     var name = req.params.name;
     res.send(`Hello ${name}`);
 });
 
+// parameters not needed with '?'
 app.get('/movies/:title?', (req, res) => {
     var title = req.params.title;
     res.send(`the title is ${title}`);
